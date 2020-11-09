96c7a829b78186d97e0b6948735a2ae517fcf6cf
LionelB <lionel@lumographe.fr>
2020-11-09 17:20:27 +0100

    feat: add index typings

--- index.d.ts
+++ index.d.ts
@@ -1,5 +1,20 @@
 export as namespace ficheVdd
 
+export type FicheIndex = {
+  id: string
+  type: "particuliers" | "associations" | "professionnels"
+  title: string
+  subject: string
+  theme: string
+  breadcrumbs: FicheVddBreadcrumbs[]
+  date: string
+}
+
+export type FicheVddBreadcrumbs = {
+  id: string
+  text: string
+}
+
 export type RawJson = {
   name: string
   text: string
